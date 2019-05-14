1bb1372935e40bd2a09b161842343a02965460d5
wang0630 <j2081499@gmail.com>
2019-05-14 11:16:54 +0800

    Add Sass

--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,54 @@
 const HtmlPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
+
 const ENV = process.env.NODE_ENV || 'development';
+const SRC_DIR = path.join(__dirname, 'src');
+const OUTPUT_DIR = path.join(__dirname, 'build');
 
 const htmlPlugin = new HtmlPlugin({
-    template: './src/index.html'
+  template: `${SRC_DIR}/index.html`,
+});
+
+const miniCssPlugin = new MiniCssExtractPlugin({
+  filename: '[name].css'
 });
 
 module.exports = {
-    watch: ENV === 'development',
-    target: 'electron-renderer',
-    entry: './src/index.js',
-    output: {
-        path: __dirname + '/build',
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.jsx?$/,
-                loader: 'babel-loader',
-                exclude: /node_modules/
-            },
-            // {
-            //     test: /\.css$/,
-            //     loader: ExtractTextPlugin.extract({
-            //       loader: 'css-loader',
-            //       options: {
-            //         modules: true
-            //       }
-            //     })
-            // },
-            {
-                test: /\.(png|jpg|gif|svg)$/,
-                loader: 'file-loader',
-                query: {
-                    name: '[name].[ext]?[hash]'
-                }
-            }
+  watch: ENV === 'development',
+  target: 'electron-renderer',
+  entry: `${SRC_DIR}/index.js`,
+  output: {
+    path: OUTPUT_DIR,
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          process.env.NODE_ENV === 'production' ? MiniCssExtractPlugin.loader : 'style-loader', // creates style nodes from JS strings
+          'css-loader', // translates CSS into CommonJS
+          'sass-loader', // compiles Sass to CSS, using Node Sass by default
         ]
-    },
-
-    plugins: [
-        htmlPlugin
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'file-loader',
+        query: {
+          name: '[name].[ext]?[hash]',
+        },
+      },
     ],
+  },
 
-    resolve: {
-      extensions: ['.js', '.json', '.jsx']
-    }
+  plugins: [
+    htmlPlugin,
+    miniCssPlugin
+  ],
 };
